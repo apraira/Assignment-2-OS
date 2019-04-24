6bc47ecad644422264a2bd57ad957f6944e32dd2
Salsa Rahmadati <43996652+salsarahmadati@users.noreply.github.com>
2019-04-24 20:04:44 +0700

    add problem link

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -8,37 +8,30 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    if num == 0{
-        return None;
-    } 
-    else if num > 0 {
-    if sum(num) == num {
-        return Some(Classification::Perfect)
-    } else if sum(num) > num {
-        Some(Classification::Abundant)
+    if num > 0 { //jika num lebih besar dari 0 maka lanjut 
+        if sum(num) == num { //cek lagi apakah summ dari faktor itu sama 
+            return Some(Classification::Perfect) }  //jika iya maka Perfect
+        else if sum(num) > num { //Jika lebih dari jumlah sum
+        Some(Classification::Abundant) //maka Abundant
+        } else { //Jika kurang dari
+        Some(Classification::Deficient) //Maka deficient
+        }
     } else {
-        Some(Classification::Deficient)
-    }
-    } else {
-        None
+        None //jika bilangan kurang dari 0 returnnya None
     }
 }
 
 pub fn sum(x:u64) -> u64{
-    let mut sum = 0;
-    if x == 1 {
-        return 0;
-    } 
-    else if x > 1 {
+    let mut sum = 0; //membuat variabel baru
         for i in 1..x{
-            if x%i == 0 {
-            sum = sum + i;
+            if x%i == 0 { //jika x habis dibagi i
+            sum = sum + i; //maka sum yang tadinya 0 ditambah dengan value dari i
             }else{
-            sum = sum;
+            sum = sum; //kalau gak habis sum tetap
             }
-            
-        }
-    }
-    sum
     
+        }
+    sum
 }
+
+// Problem link: https://exercism.io/tracks/rust/exercises/perfect-numbers/solutions/ffd4330ee24d49bca7d21d6cc310edf1
